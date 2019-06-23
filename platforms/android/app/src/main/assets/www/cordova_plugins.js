9a842b429285b98664e9f25671dc13893b0aa93e
markuskreuzinger88 <37005171+markuskreuzinger88@users.noreply.github.com>
2019-06-23 12:15:21 +0200

    fix barcodescanner plugin problems

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,19 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-barcodescanner.BarcodeScanner",
-    "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
-    "pluginId": "cordova-plugin-barcodescanner",
-    "clobbers": [
-      "cordova.plugins.barcodeScanner"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-barcodescanner": "0.7.4"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
